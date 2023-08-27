4b2e4e3631792e2ca836315d3a87688d3e11f872
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-08-27 18:19:24 +0100

    dockerfile && docker-compose added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './app/auth/auth.module';
 import { ProductModule } from './app/product/product.module';
+import { CustomerModule } from './app/customer/customer.module';
+import { OrdersModule } from './app/orders/orders.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './app/common/config/config';
+
 
 @Module({
-  imports: [AuthModule, ProductModule],
+  imports: [AuthModule, ProductModule, CustomerModule, OrdersModule, ConfigModule.forRoot({
+    load: [configuration],
+    isGlobal: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/common/config/config.ts
@@ -0,0 +1,5 @@
+export default () => ({
+    PORT: 4000,
+    JWT_SECRET: 'SECRET_TOKEN',
+    JWT_EXPIRES:''
+})
