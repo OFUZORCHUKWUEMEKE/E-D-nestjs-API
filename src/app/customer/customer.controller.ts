f6eba605d54b025fef78e6c43eef6ba81e03661c
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-09-05 22:02:01 +0100

    added

--- src/app/customer/customer.controller.ts
+++ src/app/customer/customer.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CreateCustomer } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
@@ -6,6 +6,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Customer } from './entities/customer.entity';
 import { Repository } from 'typeorm';
+import { AuthGuard } from '../common/guard/auth.gaurd';
 
 @ApiTags('customers')
 @Controller('customer')
@@ -20,6 +21,12 @@ export class CustomerController {
   @Get('/:businessname')
   async FindOne(@Param('businessname') businessname: string) {
     return this.customerService.findOne(businessname)
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('/editprofile')
+  async EditProfile(@Body() body: UpdateCustomerDto, @Req() req) {
+    return this.customerService.updateProfile(req, body)
   }
 
 
