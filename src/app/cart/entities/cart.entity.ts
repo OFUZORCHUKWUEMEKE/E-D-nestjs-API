784442bbabae0f995862ee7e7f527da9ab5ae01d
Ofuzor chukwuemeke <uniben2018@gmail.com>
2023-10-01 14:44:54 +0100

    up-to-date

--- src/app/cart/entities/cart.entity.ts
+++ src/app/cart/entities/cart.entity.ts
@@ -1,6 +1,7 @@
+import { BaseEntity } from "src/app/common/core/entity/base.entity"
 import { Customer } from "src/app/customer/entities/customer.entity"
 import { Product } from "src/app/product/entities/product.entity"
-import { BaseEntity, Entity, JoinColumn, ManyToOne } from "typeorm"
+import { CreateDateColumn, Entity, JoinColumn, ManyToOne } from "typeorm"
 
 @Entity('cart')
 export class Cart extends BaseEntity {
